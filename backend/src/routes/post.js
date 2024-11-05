b9530e55cf7a336a15178413542cc52fb4c8afe3
gusty <gustavodiorlando@gmail.com>
2024-11-05 17:25:26 -0300

    Add token validation to post routes

--- backend/src/routes/post.js
+++ backend/src/routes/post.js
@@ -2,18 +2,16 @@ const post = require('express').Router()
 const { validateToken } = require('gusty-middlewares');
 const controller = require('../controllers/postController');
 
-// require authentication for all
+post.get('/list', validateToken, controller.getPosts)
 
-post.get('/list', controller.getPosts) // list of all posts (limit and pagination!) (don't include replies unless specified, aka filter them out by default)
-
-post.get('/:postId', controller.getPostById)
-post.get('/:postId/replies', controller.getPostReplies)
-post.get('/:postId/likes', controller.getPostLikes)
+post.get('/:postId', validateToken, controller.getPostById)
+post.get('/:postId/replies', validateToken, controller.getPostReplies)
+post.get('/:postId/likes', validateToken, controller.getPostLikes)
 
 post.post('/', validateToken, controller.newPost) // post a new post
 
 post.post('/:postId/reply', validateToken, controller.replyToPost) // post a reply (which is itself a post) to a post
-post.post('/:postId/like', validateToken, controller.likePost) // like a post (or unlike!)
+post.post('/:postId/like', validateToken, controller.likePost) // like or unlike a post 
 
 post.put('/:postId') // edit a post
 
