9204eaf489a9b75fee671b1a917443d15bbfb1d5
gusty <gustavodiorlando@gmail.com>
2024-11-15 18:50:54 -0300

    Move users to right sidebar

--- frontend/src/components/layout/LeftSidebar.jsx
+++ frontend/src/components/layout/LeftSidebar.jsx
@@ -1,21 +1,14 @@
 import { useState } from "react";
 import useAuth from "../../hooks/useAuth/useAuth";
-import { useData } from "../../hooks/useData/useData"
 import { useNavigate } from "react-router-dom";
 import NewPostForm from "../post/NewPostForm";
-import Loading from '../loading/Loading';
-import FetchError from '../errors/FetchError';
 
 const buttonStyle = 'btn btn-outline-dark fw-bold rounded-0';
 
 export default function LeftSidebar() {
     const [postFormActive, setPostFormActive] = useState(false);
-    const { data: users, loading, error } = useData('user/list');
     const navigate = useNavigate();
     const auth = useAuth();
-
-    if (loading) return <Loading />
-    if (error) return <FetchError error={error} />
 
     return (
         <div className="left-sidebar">
@@ -30,14 +23,6 @@ export default function LeftSidebar() {
             <button className='btn btn-primary fw-bold rounded-0' onClick={() => setPostFormActive(true)}>
                 🖉 New Post
             </button>
-            <div>
-                <h2>Users</h2>
-                <ul>
-                    {users?.map((user) => {
-                        return <li key={user.id}>{user.displayName} @{user.username}</li>
-                    })}
-                </ul>
-            </div>
             <div className={"modal " + (postFormActive ? 'd-block' : 'd-none')}>
             {postFormActive && (
                 <NewPostForm 
--- frontend/src/components/layout/RightSidebar.jsx
+++ frontend/src/components/layout/RightSidebar.jsx
@@ -1,10 +1,32 @@
+import { useData } from "../../hooks/useData/useData";
+import Loading from "../loading/Loading";
+import FetchError from "../errors/FetchError";
+import { Link } from "react-router-dom";
+
 export default function RightSidebar() {
+    const { data: users, loading, error } = useData('user/list');
+
+    if (loading) return <Loading />
+    if (error) return <FetchError error={error} />
+
     return (
         <div className='right-sidebar'>
             <form action="">
                 <input type="text" />
                 <input type="submit" value="Search" />
             </form>
+            <div>
+                <h2>Users</h2>
+                <ul>
+                    {users?.map((user) => {
+                        return <li key={user.id}>
+                            <Link to={`/user/${user.id}`}>
+                            {user.displayName} @{user.username}
+                            </Link>
+                        </li>
+                    })}
+                </ul>
+            </div>
         </div>
     )
 }
